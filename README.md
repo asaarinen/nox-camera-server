4e944d2eb2fa99ea21510e932a25fdd55194c37a
Antti Saarinen <antti.p.saarinen@gmail.com>
2013-09-16 16:49:04 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
 nox-camera-server
-=================
+============
+
+A hobby project to store and display pictures taken by security cameras at home. Built with <a href="https://github.com/asaarinen/nox.js">Nox.js</a>.
+
+Currently being used with Axis 211 series cameras (see <a href="http://www.axis.com/products/cam_211/">here</a>), but should work with any IP cameras that can be configured to send images via HTTP POST.
+
+Installation
+--
+
+Install by cloning this repository:
+
+```
+$ git clone https://github.com/asaarinen/nox-camera-server.git
+```
+
+Install dependencies:
+
+```
+$ npm install .
+```
+
+Start the server like this:
+
+```
+$ node server.js <your-ip> [<port>]
+```
+
+where `<your-ip>` is the IP where this server can be accessed. The IP is needed in order to send notification messages with correct URLs. `port` defaults to 8080.
+
+The server will create `images` directory where all images are stored. The images older than 30 days are removed automatically.
+
+Configuring Cameras
+--
+
+You will need to configure your cameras to send images via HTTP POST. The actual url needs to be:
+
+```
+http://<your-server-ip>[:<port>]/<camera-name>/timer/image<year>-<month>-<day>_ \
+                                 <hour>-<minute>-<second>-<fraction>.jpg
+```
+
+where all `year`, `month`, etc. numbers need to have exactly 2 digits.
+
+If your camera supports motion detection, you will need to configure it to send those images to a different url. This url is similar to the regular url above, except `timer` should be replaced with `motion`:
+
+```
+http://<your-server-ip>[:<port>]/<camera-name>/motion/image<year>-<month>-<day>_ \
+                                 <hour>-<minute>-<second>-<fraction>.jpg
+```
+
+Configuring User Access
+--
+
+To secure the server, you need to give a username and password as environment variables. For example in Ubuntu Linux:
+
+```
+export cameraserverusername=<username>
+export cameraserverpasswd=<password>
+```
+
+Configuring SMS notifications
+--
+
+If you want to use <a href="http://www.twilio.com">Twilio</a> SMS service for notifications, you will need to set it up via shell environment variables before starting the server. For example in Ubuntu Linux:
+
+```
+export twilioaccountsid=<your Twilio Account SID>
+export twilioauthtoken=<your Twilio Auth Token>
+export twiliofrom=<your Twilio phone number>
+export twilioto=<your number where notification SMSes are sent>
+```
+
+The server will send SMSes at most once per hour.
+
+3rd Party Software
+--
+
+This project uses the following 3rd party software, included in the `html/` directory:
+
+- jQuery v1.10.2, Copyright 2005, 2013 jQuery Foundation, Inc. and other contributors, see license <a href="http://jquery.org/license">here</a>
+- Bootstrap v3.0, Copyright 2013 Twitter, Inc under the Apache 2.0 license, see license <a href="https://github.com/twbs/bootstrap/blob/master/LICENSE">here</a>
+- respond.min.js, Copyright 2011: Scott Jehl, scottjehl.com, <a href="http://opensource.org/licenses/mit-license.php">MIT licensed</a>
+- html5shiv, see <a href="https://code.google.com/p/html5shiv/">here</a>, <a href="http://opensource.org/licenses/mit-license.php">MIT licensed</a>
+
+License
+--
+
+(The MIT License)
+
+Copyright (c) 2013 Antti Saarinen &lt;antti.p.saarinen@gmail.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ server.js
@@ -0,0 +1,186 @@
+
+var express = require('express');
+var path = require('path');
+var async = require('async');
+var childp = require('child_process');
+var fs = require('fs');
+
+function log(str) {
+    var d = new Date();
+    process.stderr.write(d.toISOString() + ' ' + str + '\n');
+}
+
+var domain = process.argv[2];
+var port = 8080;
+
+var twclient = null;
+if( typeof domain == 'string' &&
+    process.env['twilioaccountsid'] &&
+    process.env['twilioauthtoken'] &&
+    process.env.twiliofrom &&
+    process.env.twilioto ) {
+	twclient = require('twilio')(process.env['twilioaccountsid'],
+				     process.env['twilioauthtoken']);
+    log('using twilio');
+}
+
+log('creating nox app');
+var noxapp = require('nox-app-template')('nox-camera-server', 
+					 function(mod) { return require(mod); }, log);
+
+noxapp.expressServer.use(function(req, res, next) {
+    if( req.session.username ||
+	( req.method == 'POST' && ( req.url.match(/^\/motion\//) ||
+				    req.url.match(/^\/timer\//) ) ) ||
+	( req.method == 'GET' && ( req.url == '/login.html' ||
+				   req.url.match(/^\/css\//) ||
+				   req.url.match(/^\/js\//) ||
+				   req.url.match(/^\/fonts\//) ||
+				   req.url.match(/^\/ico\//) ) ) )
+	next();
+    else
+	res.redirect('/login.html');
+});
+
+noxapp.noxApp.page('/login.html', [ './lib/auth.js' ], [ './lib/login.js' ]);
+noxapp.noxApp.page('/pics.html', [ './lib/auth.js', './lib/api.js' ], [ './lib/pics.js' ]);
+
+noxapp.expressServer.get(/\/pics\//, function(req, res) {
+    var path = 'images/' + req.url.substring(6);
+    fs.exists(path, function(exists) {
+	if( exists ) {
+	    res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+	    fs.createReadStream(path).pipe(res);
+	} else {
+	    res.writeHead(404);
+	    res.end();
+	}
+    });
+});
+
+var lastnotification = -1;
+
+noxapp.expressServer.post('/motion/:cameraid', function(req, res) {
+    postImage(req, res, true);
+});
+
+noxapp.expressServer.post('/timer/:cameraid', function(req, res) {
+    postImage(req, res, false);
+});
+
+function postImage(req, res, motion) {
+
+    var disposition = req.headers['content-disposition'];
+    log('posting image ' + disposition);
+
+    var m = disposition.match(/attachment; filename="([^"]+)"/)[1];
+
+    if( m.match(/^([0-9]{2})-([0-9]{2})-([0-9]{2})_([0-9]{2})-([0-9]{2})-([0-9]{2})-([0-9]{2})$/) ) {
+	log('non-prefixed disposition: ' + m);
+	m = 'image' + m + '.jpg';
+    } else if( !m.match(/\.jpg$/) ) {
+	log('weird disposition: ' + m);
+	m = m + '.jpg';
+    }
+
+    var dir1 = req.params.cameraid;
+    var dir2 =  (motion ? 'motion' : 'timer')
+    var path = 'images/' + dir1 + '/' + dir2 + '/' + m;
+	
+    async.waterfall([
+	function(wfcb) {
+	    fs.mkdir('images/' + dir1, function(err) { wfcb(); })
+	},
+	function(wfcb) {
+	    fs.mkdir('images/' + dir1 + '/' + dir2,
+		     function(err) { wfcb(); });
+	},
+	function(wfcb) {
+	    log('writing to ' + path);
+	    var fout = fs.createWriteStream(path);
+	    req.pipe(fout);
+	    fout.on('close', function(err) {
+		wfcb(err);
+	    });
+	}
+    ], function(err) {
+	if( err ) {
+	    log('error processing request');
+	    res.writeHead(500);
+	    res.end();
+	} else {
+	    log('successful upload');
+	    res.writeHead(200);
+	    res.end();
+
+	    if( motion ) {	    // notify
+
+		var now = new Date();
+		now = now.getTime();
+		if( lastnotification == -1 || 
+		    now - lastnotification > 60 * 60 * 1000 ) {
+		    
+		    lastnotification = now;
+		    
+		    var notify = 'Alert: http://' + domain + ':' + port + 
+			'/pics/' + path.substring(7);
+		    log('notification: ' + notify);
+		    
+		    if( twclient ) {
+			twclient.sendSms({
+			    to: process.env.twilioto, 
+			    from: process.env.twiliofrom,
+			    body: notify
+			}, function(err, response) {
+			    if( err )
+				log('error sending message: ' + 
+				    JSON.stringify(err));
+			    else
+				log('twilio success response');
+			});
+		    }
+		}
+	    }
+	}
+    });
+}
+
+noxapp.expressServer.use(express.static('html'));
+
+noxapp.expressServer.use(function(req, res, next) {
+    // not found
+    res.redirect('/pics.html');
+});
+
+var api = require('./lib/api.js');
+
+async.waterfall([
+    function(wfcb) { fs.mkdir('images', function(err) { wfcb(); }) },
+    function(wfcb) {
+	if( typeof process.argv[3] == 'string' )
+	    if( process.argv[3].match(/^[0-9]+$/) )
+		port = parseInt(process.argv[3]);
+	
+	log('starting server at port ' + port);
+	var serverinst = noxapp.httpServer.listen(port);
+
+	var cleaner = setTimeout(api.clean, 24 * 60 * 60 * 1000);
+	
+	process.on('SIGINT', function() {
+	    log('stopping server gracefully');
+	    serverinst.close();
+	    log('stopping interval timer');
+	    clearTimeout(cleaner);
+	    process.on('exit', function() {
+		log('server stopped gracefully');
+	    });
+	});
+	wfcb();
+    }
+], function(err) {
+    if( err ) {
+	log('error starting server: ' + err);
+	process.exit(1);
+    } else
+	log('listening at ' + port);
+});
